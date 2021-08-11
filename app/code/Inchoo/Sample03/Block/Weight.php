b9c742bb8fa550dcbe6baae96e2db82027014463
jlboban <jlboban17@gmail.com>
2021-08-11 13:21:22 +0200

    Code check handling

--- app/code/Inchoo/Sample03/Block/Weight.php
+++ app/code/Inchoo/Sample03/Block/Weight.php
@@ -1,12 +1,11 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Inchoo\Sample03\Block;
 
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 
+// @codingStandardsIgnoreFile
 class Weight extends Template
 {
     /**
