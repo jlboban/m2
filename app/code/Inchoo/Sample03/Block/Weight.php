77a3bd17a7f86a7b882480229b5ca21fd1f7fe28
jlboban <jlboban17@gmail.com>
2021-08-11 12:39:53 +0200

    Sample03 tasks

---
+++ app/code/Inchoo/Sample03/Block/Weight.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inchoo\Sample03\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+
+class Weight extends Template
+{
+    /**
+     * Weight constructor.
+     * @param Context $context
+     */
+    public function __construct(Context $context)
+    {
+        parent::__construct($context);
+    }
+}
