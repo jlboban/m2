8a17d031c213019abbd25db9bb01688f2825141c
jlboban <jlboban17@gmail.com>
2021-09-10 11:25:03 +0200

    Created new data patch for updating oib customer attribute

--- app/code/Inchoo/Sample07/Setup/Patch/Data/UpdateOibCustomerAttribute.php
+++ app/code/Inchoo/Sample07/Setup/Patch/Data/UpdateOibCustomerAttribute.php
@@ -9,7 +9,7 @@ use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 
-class CreateOibCustomerAttribute implements DataPatchInterface
+class UpdateOibCustomerAttribute implements DataPatchInterface
 {
     /**
      * @var EavSetupFactory
@@ -39,7 +39,9 @@ class CreateOibCustomerAttribute implements DataPatchInterface
      */
     public static function getDependencies()
     {
-        return [];
+        return [
+            CreateOibCustomerAttribute::class
+        ];
     }
 
     /**
@@ -60,25 +62,39 @@ class CreateOibCustomerAttribute implements DataPatchInterface
 
         $attributeCode = 'oib';
         $entityType = CustomerMetadataInterface::ENTITY_TYPE_CUSTOMER;
-        $setId = CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER;
-
-        $eavSetup->addAttribute($entityType, $attributeCode, [
-            'type'           => 'varchar',
-            'input'          => 'text',
-            'label'          => 'OIB',
-            'required'       => 0,
-            'user_defined'   => 1, // required for custom attributes
-            'system'         => 0, // required for custom attributes
-            'sort_order'     => 95, // position in a attribute group
-            'position'       => 95, // position in a adminhtml form
-            'validate_rules' => '{"input_validation":"other","max_text_length":11,"validate-digits":true}'
-        ]);
-
         $attributeId = (int)$eavSetup->getAttributeId($entityType, $attributeCode);
 
-        $defaultGroupId = $eavSetup->getDefaultAttributeGroupId($entityType, $setId);
-        $eavSetup->addAttributeToSet($entityType, $setId, $defaultGroupId, $attributeId);
+        $eavSetup->updateAttribute($entityType, $attributeCode, [
+            'is_used_in_grid' => 1,
+            'is_filterable_in_grid' => 1
+        ]);
+
+        $this->createFormAttributeRelations($attributeId);
 
         return $this;
+    }
+
+    /**
+     * @param int $attributeId
+     * @return void
+     */
+    protected function createFormAttributeRelations(int $attributeId): void
+    {
+        $formAttributeTable = $this->moduleDataSetup->getTable('customer_form_attribute');
+
+        $this->moduleDataSetup->getConnection()->delete($formAttributeTable, ['attribute_id = ?' => $attributeId]);
+
+        $usedInForms = [
+            'customer_account_create',
+            'customer_account_edit',
+            'adminhtml_customer'
+        ];
+
+        $formAttributeData = [];
+        foreach ($usedInForms as $formCode) {
+            $formAttributeData[] = ['form_code' => $formCode, 'attribute_id' => $attributeId];
+        }
+
+        $this->moduleDataSetup->getConnection()->insertMultiple($formAttributeTable, $formAttributeData);
     }
 }
